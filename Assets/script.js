f201208463d0c10266f0cbdc087458982debbebc
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-05-08 22:30:55 -0500

    added code for the quiz results page and the high scores page. These sections do not have full functionality yet, just the basic framework.

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
 var q1 = document.querySelector(".question-1");
-
+var submitbtn = document.querySelector(".submit");
 // Start Quiz and clear intro page
 
 var startbtn = document.querySelector('.intro button');
@@ -12,18 +12,35 @@ startbtn.addEventListener("click", function() {
 
 function selectAnswer(event) {
     var questionNumber = Number(event.target.parentElement.id[event.target.parentElement.id.length-1]);
-    var nextQuestion = questionNumber + 1;
-    
-    document.querySelector('.question-' + questionNumber).style.display = "none";
-    document.querySelector('.question-' + nextQuestion).style.display = "block";
-    if(event.target.className == "correct") {
-        document.querySelector("#status-" + questionNumber).textContent = "Correct!";
+    if(questionNumber < 5) {
+        var nextQuestion = questionNumber + 1;
+        document.querySelector('.question-' + questionNumber).style.display = "none";
+        document.querySelector('.question-' + nextQuestion).style.display = "block";
+        if(event.target.className == "correct") {
+            document.querySelector("#status-" + questionNumber).textContent = "Correct!";
+        } else {
+            document.querySelector("#status-" + questionNumber).textContent = "Inorrect!";
+        }
     } else {
-        document.querySelector("#status-" + questionNumber).textContent = "Inorrect!";
+        if(event.target.className == "correct") {
+            document.querySelector("#status-5").textContent = "Correct!";
+        } else {
+            document.querySelector("#status-5").textContent = "Inorrect!";
+        }
+        document.querySelector(".question-5").style.display = "none";
+        document.querySelector(".results").style.display = "block";
     }
+    
 }
 
 var answers = document.querySelectorAll('span');
 for(let i = 0; i < answers.length; ++i) {
     answers[i].addEventListener("click", selectAnswer);
 }
+
+function showScores(event) {
+    document.querySelector(".results").style.display = "none";
+    document.querySelector('.highscores').style.display = "block";
+}
+
+submitbtn.addEventListener('click', showScores);
