107a21cb689efa2d47c54252dd8f4f62362b5d6d
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-05-14 01:18:41 -0500

    added 'View High Scores' functionality.

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,10 @@
 var q1 = document.querySelector(".question-1");
 var submitbtn = document.querySelector(".submit");
 var clearscoresbtn = document.querySelector('.clear-score');
-var time = 20;
+var time = 50;
 document.querySelector('#timer').textContent = "Time: " + time;
 var scorelist = document.querySelector('.score-list');
+var viewscores = document.querySelector('#view-scores');
 
 
 // Start Quiz and clear intro page
@@ -57,7 +58,7 @@ if(event.target.className == "correct") {
 }
 
 
-function selectAnswer(event) {
+function selectAnswer(event) { //fix this
     var questionNumber = Number(event.target.parentElement.id[event.target.parentElement.id.length-1]);
     if(questionNumber < 5) {
         var nextQuestion = questionNumber + 1;
@@ -81,7 +82,7 @@ for(let i = 0; i < answers.length; ++i) {
 
 function showScores(event) {
     document.querySelector(".results").style.display = "none";
-    document.querySelector('.highscores').style.display = "block";
+    document.querySelector('#highscores').style.display = "block";
     var key = document.querySelector('#name').value;
     localStorage.setItem(key, time);
     var names = sortStorage();
@@ -125,6 +126,17 @@ function clearScores() {
     scorelist.textContent = "";
 }
 
+function viewScoreList() {
+    document.querySelectorAll('main > section').forEach(function(section){
+        section.style.display = "none";
+    });
+    var names = sortStorage();
+    displayStorage(names);
+    document.querySelector('#highscores').style.display = "block";
+    viewscores.disabled = true;
+}
+
 
 submitbtn.addEventListener('click', showScores);
 clearscoresbtn.addEventListener('click', clearScores);
+viewscores.addEventListener('click', viewScoreList);
