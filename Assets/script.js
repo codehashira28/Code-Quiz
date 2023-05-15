87de2911feab1757d136f2b9ebf49dab502b55bd
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-05-14 21:35:03 -0500

    added comments to explain code and consdolidated some CSS

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,15 @@
 var q1 = document.querySelector(".question-1");
 var submitbtn = document.querySelector(".submit");
 var clearscoresbtn = document.querySelector('.clear-score');
-var time = 70;
+var time = 50;
 document.querySelector('#timer').textContent = "Time: " + time;
 var scorelist = document.querySelector('.score-list');
 var viewscores = document.querySelector('#view-scores');
-
-
-// Start Quiz and clear intro page
-
 var clock;
+
+// Start Quiz and clear intro page. Display the first question and start timer.
+// Conditions put in place that if timer hits zero or below zero, set time back to zero and display the results page with score of 0.
+
 
 var startbtn = document.querySelector('.intro button');
 startbtn.addEventListener("click", function() {
@@ -31,6 +31,9 @@ startbtn.addEventListener("click", function() {
         }
         }, 1000);
 });
+
+//function that runs in response to selected answers. Program will display "Correct!" if user gets the answer right.
+//If the user selects an incorrect answer, the program will display "Incorrect!" and deduct 10 seconds from timer.
 
 function showStatus(event, questionNumber) {
 if(event.target.className == "correct") {
@@ -58,8 +61,10 @@ if(event.target.className == "correct") {
   }
 }
 
+//function that responds to user selected answer of current question and proceeds to the next question or to the results page if the last question was answered.
+//function will also show whether the answer was correct or incorrect from 'showStatus' function.
 
-function selectAnswer(event) { //fix this
+function selectAnswer(event) { 
     var questionNumber = Number(event.target.parentElement.id[event.target.parentElement.id.length-1]);
     if(questionNumber < 5) {
         var nextQuestion = questionNumber + 1;
@@ -76,10 +81,14 @@ function selectAnswer(event) { //fix this
    
 }
 
+// adds the capability of all multiple choice answers to be selectable and run the 'selectAnswer' function
+
 var answers = document.querySelectorAll('span');
 for(let i = 0; i < answers.length; ++i) {
     answers[i].addEventListener("click", selectAnswer);
 }
+
+//once the user inputs their initials and submits, the highscores page will render and show the high scores of all participants in order of highest scores.
 
 function showScores(event) {
     document.querySelector(".results").style.display = "none";
@@ -91,6 +100,8 @@ function showScores(event) {
     displayStorage(names);
 }
 
+//function that displays the scores of the participants in an ordered list.
+
 function displayStorage(names) {
     for(var i = names.length-1; i >= 0; --i) {
         var entry = document.createElement('li');
@@ -100,6 +111,11 @@ function displayStorage(names) {
         scorelist.appendChild(entry);
     }  
 }
+
+//function that takes the scores of participants in the localStorage and orders them by highest score.
+//the scores were put in an array and sorted (sort code taken from online source) and then matched back to their respective users.
+//the names were then placed in a separate array in the sorted order based on scores and then rendered to the highscores list.
+
 
 function sortStorage() {
     var scores = [];
@@ -123,10 +139,16 @@ function sortStorage() {
 
 }
 
+//function to clear all of the scores from storage and highscores list reset.
+
 function clearScores() {
     localStorage.clear();
     scorelist.textContent = "";
 }
+
+//function that allows the user to see the highscores list at any time. 
+//all questions displays had to be set to none and then render the highscores page.
+//the 'View High Scores' button had to be set to disabled when viewing the high scores list to avoid double printing scores.
 
 function viewScoreList() {
     document.querySelectorAll('main > section').forEach(function(section){
@@ -138,6 +160,7 @@ function viewScoreList() {
     viewscores.disabled = true;
 }
 
+//event listeners were added to the submit button for submitting participant scores, clearing the scores list, and viewing the scores page.
 
 submitbtn.addEventListener('click', showScores);
 clearscoresbtn.addEventListener('click', clearScores);
